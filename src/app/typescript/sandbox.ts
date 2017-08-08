99de28a87ab50710db99127205da060de5ec7cf2
Michael Treser <armikael2@gmail.com>
2017-08-08 09:12:32 +0300

    TypeScript classes, properties, access modifiers, getter and setter.

--- src/app/typescript/sandbox.ts
+++ src/app/typescript/sandbox.ts
@@ -12,8 +12,9 @@ let backgroundColor = Color.Blue;
 
 // Type Assertions
 // To get prompts with a list of methods, you need to set the variable type next to it.
+let message;
 let userName = (<string>message).lastIndexOf('Mr');
-let name = (message as string).indexOf('Mr');
+let userParams = (message as string).indexOf('Mr');
 
 
 // Arrow functions
@@ -31,6 +32,37 @@ interface Point {
     y: number
 }
 
-let drawPoint = (point: Point) => {
+let getPoint = (point: Point) => {
 
 };
+
+// Classes
+class PointClass {
+    // Access modifiers: public, private, protected
+    // private - access modifier that doesn't allows to change variable from outside the class
+    // ? after the initial variable tell to compiler that this is optional parameter
+    constructor(private _x?: number, private _y?: number) { 
+
+    }
+
+    draw() {
+        console.log('x: ' + this._x + ', y: ' + this._y);
+    }
+
+    // X Propertie - Getter
+    get x() {
+        return this._x;
+    }
+
+    // Y Propertie - Setter
+    set x(value) {
+        if (value < 0) {
+            throw new Error('value cannot be less than 0.');
+        }
+    }
+}
+
+let point = new PointClass(1, 2);
+point.x = 20;
+point.draw();
+
